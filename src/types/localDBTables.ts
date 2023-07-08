83da0d296f142a66583d5b9dd0d18b1daacc40d3
carsonkrueger <crkruege@gmail.com>
2023-07-07 22:31:29 -0600

    added workout table row type, parsed & unparsed

--- src/types/localDBTables.ts
+++ src/types/localDBTables.ts
@@ -1,10 +1,18 @@
 import { EntityState } from "@reduxjs/toolkit";
 import { Exercise, WorkoutSet, WorkoutState } from "./workoutTypes";
 
-export interface workoutsTableRow {
+export interface parsedWorkoutsTableRow {
   workout_id: number;
   workout_state: WorkoutState;
   exercises: EntityState<Exercise>;
   sets: EntityState<WorkoutSet>;
+  last_performed: string;
+}
+
+export interface unparsedWorkoutsTableRow {
+  workout_id: string;
+  workout_state: string;
+  exercises: string;
+  sets: string;
   last_performed: string;
 }
